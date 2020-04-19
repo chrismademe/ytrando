07b489ca4df778573a475397226ec649058dc067
Chris <chris@underscores.co.uk>
2020-04-19 12:45:30 +0100

    Add PurgeCSS

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ const plumber = require('gulp-plumber');
 const autoprefixer = require('autoprefixer');
 const postcss = require('gulp-postcss');
 const tailwind = require('tailwindcss');
+const purgecss = require('gulp-purgecss');
 
 /**
  * Default Task that runs when you type gulp in the console
@@ -26,24 +27,39 @@ const defaultTask = function (done) {
  */
 const compileCSS = function () {
     // Load Files
+    return gulp
+        .src(`${config.source}/style.css`)
+        .pipe(postcss([tailwind, autoprefixer]))
+        .pipe(gulp.dest(config.dest));
+    // .pipe(notify({ message: 'Styles task complete' }))
+};
+
+const buildCSS = function () {
+    // Load Files
     return (
         gulp
             .src(`${config.source}/style.css`)
 
             // Autoprefix and Minify
             .pipe(postcss([tailwind, autoprefixer]))
+
+            .pipe(
+                purgecss({
+                    content: ['src/*.html', 'src/assets/js/*.js'],
+                    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+                })
+            )
+
             .pipe(minifycss())
 
-            // Save it and update the browser
+            // Save it
             .pipe(gulp.dest(config.dest))
-        // .pipe(notify({ message: 'Styles task complete' }))
     );
 };
 
 function watch(done) {
     // Watch .scss files
     gulp.watch(`${config.source}/*.css`, compileCSS);
-
     done();
 }
 
@@ -52,3 +68,4 @@ const watchAndSync = gulp.parallel(watch);
 exports.default = defaultTask;
 exports.compileCSS = compileCSS;
 exports.watch = watchAndSync;
+exports.buildCSS = buildCSS;
